0e68b583a8b0d9285f5daad32323acb89378a279
micah810 <micahlarson810@gmail.com>
2024-02-23 11:36:32 -0600

    creating how.html.erb & a new view that shows the title and body of a single article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,11 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
+
+  def show # this method handles requests to view a specific article, identified by its id parameter
+    @article = Article.find(params[:id])
+  end
+
+
 end
